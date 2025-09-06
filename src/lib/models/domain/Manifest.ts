544ceaf40bf833cbd85b7eac49c4ff8b42739df3
ChaosCantrip <dev@chaoscantrip.com>
2025-09-06 05:11:44 +0100

    Added @lib/models/domain/Manifest

---
+++ src/lib/models/domain/Manifest.ts
@@ -0,0 +1,24 @@
+import { Card, Set, Rarity } from "@lib/models/domain";
+
+export class Manifest {
+    private static _initialised = false;
+
+    public static isInitialised(): boolean {
+        return this._initialised;
+    }
+
+    public static async initialise() {
+        if (this._initialised) {
+            console.log("Manifest is already initialised.");
+            return;
+        }
+        console.log("Initialising manifest...");
+
+        await Card.initialise();
+        await Set.initialise();
+        await Rarity.initialise();
+
+        console.log("Manifest initialised.");
+        this._initialised = true;
+    }
+}
--- src/lib/models/domain/index.ts
+++ src/lib/models/domain/index.ts
@@ -1,3 +1,4 @@
 export * from './Set';
 export * from './Rarity';
 export * from './Card';
+export * from './Manifest';
