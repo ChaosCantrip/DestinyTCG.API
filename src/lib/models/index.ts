15c002cd7107defdf86784d2f464c2553798b2b3
ChaosCantrip <dev@chaoscantrip.com>
2025-09-06 07:15:37 +0100

    Added APIResponse

--- src/lib/models/index.ts
+++ src/lib/models/index.ts
@@ -2,3 +2,4 @@ export * as Domain from "./domain";
 export * as Events from "./events";
 export * as Errors from "./errors";
 export * as Firestore from "./firestore";
+export * as Responses from "./responses";
---
+++ src/lib/models/responses/APIResponse.ts
@@ -0,0 +1,66 @@
+import { NextResponse } from "next/server";
+import { IncompleteResponseError } from "@models/errors";
+
+type success = "success" | "error";
+
+interface Meta {
+    timestamp: string;
+    processingTime: number;
+}
+
+interface APIResponseData<T> {
+    success: success;
+    message: string;
+    data: T | null;
+    meta: Meta;
+}
+
+export class APIResponse<T>
+{
+    private _success: boolean | null = null;
+    message: string | null = null;
+    data: T | null = null;
+    startTime: Date;
+
+    constructor(startTime: Date)
+    {
+        this.startTime = startTime;
+    }
+
+    public toJSON(): APIResponseData<T>
+    {
+        const endTime = new Date();
+        const processingTime = endTime.getTime() - this.startTime.getTime();
+
+        if (this._success === null)
+        {
+            throw new IncompleteResponseError("success");
+        }
+        if (this.message === null)
+        {
+            throw new IncompleteResponseError("message");
+        }
+
+        return {
+            success: this._success ? "success" : "error",
+            message: this.message,
+            data: this.data,
+            meta: {
+                timestamp: endTime.toISOString(),
+                processingTime
+            }
+        };
+    }
+
+    public success(status: number): NextResponse
+    {
+        this._success = true;
+        return NextResponse.json(this.toJSON(), { status });
+    }
+
+    public error(status: number): NextResponse
+    {
+        this._success = false;
+        return NextResponse.json(this.toJSON(), { status });
+    }
+}
