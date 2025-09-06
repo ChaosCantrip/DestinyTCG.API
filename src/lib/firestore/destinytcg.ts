0d0ed10a047ac1b5a1993c080cda3488db170e5d
ChaosCantrip <dev@chaoscantrip.com>
2025-09-06 02:57:42 +0100

    Added Firestore.DestinyTCG.getAllRarities()

--- src/lib/firestore/destinytcg.ts
+++ src/lib/firestore/destinytcg.ts
@@ -1,1 +1,10 @@
-export const temp = "temp";
+import { RarityFirestoreData } from "@models/firestore";
+import { collection, getDocs } from "firebase/firestore";
+import { Firestore } from "@lib/firestore";
+
+export async function getAllRarities(): Promise<RarityFirestoreData[]> {
+    const raritiesCollection = collection(Firestore.db, "rarities");
+    const snapshot = await getDocs(raritiesCollection);
+    const rarities: RarityFirestoreData[] = snapshot.docs.map(doc => doc.data() as RarityFirestoreData);
+    return rarities;
+}
