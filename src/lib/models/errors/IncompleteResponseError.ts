eae202545db951fa542d55beb2919357c112e973
ChaosCantrip <dev@chaoscantrip.com>
2025-09-06 07:30:21 +0100

    Fixed IncompleteResponseError

--- src/lib/models/errors/IncompleteResponseError.ts
+++ src/lib/models/errors/IncompleteResponseError.ts
@@ -1,8 +1,11 @@
 export class IncompleteResponseError extends Error
 {
+    missingPart: string;
+
     constructor(missingPart: string) 
     {
         super(`Response is missing the following part: ${missingPart}`);
         this.name = "IncompleteResponseError";
+        this.missingPart = missingPart;
     }
 }
