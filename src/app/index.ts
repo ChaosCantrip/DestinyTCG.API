5bc9a86e6a979ae5ab10e49f2e46d56c70abc337
ChaosCantrip <dev@chaoscantrip.com>
2025-09-07 22:49:48 +0100

    Added initial project run setup

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,12 +1,33 @@
 import express from "express";
 import endpointsRouter from "./endpoints";
+import "dotenv/config";
+import { Manifest } from "../lib/models/domain";
 
 const app = express();
 const port = process.env.PORT || 3002;
 
-app.use("/api", endpointsRouter);
-
-app.listen(port, () => 
+async function initialiseApp()
 {
-    console.log(`API is running at http://localhost:${port}`);
-});
+    console.log("===== Initialising application... =====");
+    await Manifest.initialise();
+    console.log("===== Application initialised. =====");
+}
+
+async function main() 
+{
+    console.log("===== Starting API server... =====");
+    app.use("/api", endpointsRouter);
+
+    app.listen(port, () => 
+    {
+        console.log(`API is running at http://localhost:${port}`);
+    });
+}
+
+async function start()
+{
+    await initialiseApp();
+    await main();
+}
+
+start();
