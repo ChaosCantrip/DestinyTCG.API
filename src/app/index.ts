3a604e2195673c548a2d718a7ddd7e424785df19
ChaosCantrip <dev@chaoscantrip.com>
2025-09-11 22:28:03 +0100

    Improved main() terminal output

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,27 +1,28 @@
 import express from "express";
 import endpointsRouter from "./endpoints";
 import { Manifest } from "../lib/models/domain";
+import chalk from "chalk";
 
 const app = express();
 const port = process.env.PORT || 3002;
 
 async function initialiseApp()
 {
-    console.log("===== Initialising application... =====");
+    console.log(chalk.yellow("\n===== Initialising application... ====="));
     await Manifest.initialise();
-    console.log("===== Application initialised. =====");
+    console.log(chalk.green("===== Application initialised. ====="));
 }
 
 async function main() 
 {
-    console.log("===== Starting API server... =====");
+    console.log(chalk.yellow("\n===== Starting API server... ====="));
 
     app.use(express.json());
     app.use("/api", endpointsRouter);
 
     app.listen(port, () => 
     {
-        console.log(`API is running at http://localhost:${port}`);
+        console.log(chalk.green(`API is running at http://localhost:${port}`));
     });
 }
 
