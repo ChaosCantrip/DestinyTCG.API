0c8c0f83d4ad57b8f6d702a974bae48b95b4cd2e
ChaosCantrip <dev@chaoscantrip.com>
2025-09-12 15:05:48 +0100

    Added "/auth" router

--- src/app/endpoints/index.ts
+++ src/app/endpoints/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import pingRouter from "./ping";
 import cardsRouter from "./cards";
+import authRouter from "./auth";
 const router = express.Router();
 
 router.use("/ping", pingRouter);
 router.use("/cards", cardsRouter);
+router.use("/auth", authRouter);
 
 export default router;
