6eb9fbc6d823739449d75e554597ccbeb41a0b2e
ChaosCantrip <dev@chaoscantrip.com>
2025-09-12 16:07:00 +0100

    Added placeholder /auth/refresh endpoint

--- src/app/endpoints/auth/index.ts
+++ src/app/endpoints/auth/index.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import bungieRouter from "./bungie";
+import refreshHandler from "./refresh";
 
 const authRouter = express.Router();
 
 authRouter.use("/bungie", bungieRouter);
+authRouter.post("/refresh", refreshHandler);
+
 
 export default authRouter;
